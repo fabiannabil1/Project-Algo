7924adaeb838cff643162fddc18cd52481dabdd8
fabiannabil1 <fabiannabil00@gmail.com>
2023-11-03 21:33:40 +0700

    update login
    update register dan perbaikan login

--- LOGIN/Login.py
+++ LOGIN/Login.py
@@ -4,20 +4,26 @@ def clear_console():
     os.system('cls')
 
 def Register():
-    regist =[input('Masukkan Username :'),
-            input('Masukkan Password :')]
+    regist =[input('Masukan Nama Anda\t:'),
+             input('Masukkan Pekerjaan Anda\t:'),
+             input('Masukkan Username\t:'),
+             input('Masukkan Password\t:')]
     with open('login.csv','w') as register:
-        user_data = f"{regist[0]},{regist[1]}"
+        user_data = f"{regist[0]},{regist[1]},{regist[2]},{regist[3]}"
         register.write(user_data)
     register.close()
-    print('Username dan Password tersimpan')
-    input('enter untuk melanjutkan')
+    yakin_tidak = input('Apakah anda yakin? (y/t):')
+    if yakin_tidak == 'y':
+        print('Username dan Password tersimpan')
+        input('enter untuk melanjutkan')
+    else:
+        Register()
 
 def login_user(username,password):
     with open('login.csv','r') as file:
         data = file.read()
         data = data.split(',')
-        if username == data[0] and password == data[1]:
+        if username == data[2] and password == data[3]:
             print('Login Berhasil')
         else:
             print('Login Gagal')
@@ -30,17 +36,17 @@ def ganti_userpass():
     with open('login.csv','r') as file:
         data_user = file.read()
         data_user = data_user.split(',')
-        if check[0] == data_user[0] and check[1] == data_user[1]:
+        if check[0] == data_user[2] and check[1] == data_user[3]:
             akun_baru = [input('Masukkan Username baru :'),
                          input('Masukkan Password baru :')]
             with open('login.csv','w') as user_baru:
-                simpan_baru = f'{akun_baru[0]},{akun_baru[1]}'
+                simpan_baru = f'{data_user[0]},{data_user[1]},{akun_baru[0]},{akun_baru[1]}'
                 user_baru.write(simpan_baru)
         else:
             print('Username atau Password yang anda masukkan salah')
             print('1. Kembali ke login')
             print('2. Ulangi mengubah akun')
-            pilihan_ubah = int(input('Pilihan anda :'))
+            pilihan_ubah = int(input('Ketikkan pilihan anda :'))
             match pilihan_ubah:
                 case 1:
                     exit
@@ -61,17 +67,22 @@ while True:
             clear_console()
             with open('Tampilan Login.txt','r') as gui:
                 first_gui = gui.read()
-                
                 print(first_gui)
-
-            pilihan_awal = int(input('Ketikkan Pilihan Anda\t:'))
+            with open('login.csv','r') as welcomings:
+                welcoming = welcomings.read()
+                Profil_Pengguna = welcoming.split(',')
+                welcome = f"{'Selamat Datang'} {Profil_Pengguna[0]}"
+                print(f"{welcome:^68}")
+            pilihan_awal = (input('Ketikkan Pilihan Anda\t:'))
             match pilihan_awal:
-                case 1 :
+                case '1' :
                     username_login = [input('Masukkan Username\t:'),
                                     input('Password \t\t:')]
                     login_user(username_login[0],username_login[1])
-                case 2 :
+                case '2' :
                     ganti_userpass()
-                case 3 :
+                case '3' :
                     clear_console()
                     break
+                case _ :
+                    pass
